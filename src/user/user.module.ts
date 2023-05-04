ecb9076249785fb700a3e79a72172c2e654f196e
Milan <pro@turtletv.fr>
2023-05-04 10:42:47 +0200

    ✨ Implement confirmation email on register

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,12 +5,14 @@ import { AuthModule as AuthApiModule, } from 'src/api/auth/auth.module';
 import { AuthApiProvider } from 'src/api/auth/auth.service';
 import { UserApiProvider } from 'src/api/auth/user/user.service';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from 'src/auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
+import { NotificationsApiProvider } from 'src/api/notifications/notifications.service';
+import { EmailApiProvider } from 'src/api/notifications/mails/mails.service';
+import { NotificationsModule } from 'src/api/notifications/notifications.module';
 
 @Module({
-  imports: [ConfigModule, AuthApiModule],
+  imports: [ConfigModule, AuthApiModule, NotificationsModule],
   controllers: [UserController],
-  providers: [UserService, AuthApiProvider, UserApiProvider, AuthService]
+  providers: [UserService, AuthService, AuthApiProvider, UserApiProvider, NotificationsApiProvider, EmailApiProvider]
 })
 export class UserModule { }
