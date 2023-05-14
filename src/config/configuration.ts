6e723961066e3e90b18cecae8fd28d9c9ea2f7e4
Milan <pro@turtletv.fr>
2023-05-14 19:24:02 +0200

    🔧 Edit config for k8s

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
   app_port: 4000 ?? process.env.APP_PORT,
   service: "ffc-main-service",
-  issuer: "http://localhost:4000" ?? process.env.ISSUER,
+  issuer: "http://api.ffc.mistergooddeal.org" ?? process.env.ISSUER,
   authorizedServices: [
     "ffc-analytics-service",
     "ffc-auth-service",
@@ -10,8 +10,8 @@ export default () => ({
     "ffc-payments-service",
   ],
   ffc_analytics_url: "http://localhost:4001" ?? process.env.FFC_ANALYTICS_URL,
-  ffc_auth_url: "http://localhost:4002" ?? process.env.FFC_AUTH_URL,
-  ffc_main_url: "http://localhost:4000" ?? process.env.FFC_MAIN_URL,
+  ffc_auth_url: "http://10.102.3.34:4002" ?? process.env.FFC_AUTH_URL,
+  ffc_main_url: "http://api.ffc.mistergooddeal.org" ?? process.env.FFC_MAIN_URL,
   ffc_notifications_url:
     "http://localhost:4003" ?? process.env.FFC_NOTIFICATIONS_URL,
   ffc_payments_url: "http://localhost:4004" ?? process.env.FFC_PAYMENTS_URL,
