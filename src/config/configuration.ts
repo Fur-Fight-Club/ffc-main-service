f820834add70c2bf25a3a89e6da97c401912cde0
Milan <pro@turtletv.fr>
2023-05-14 21:50:33 +0200

    ✨ Add `ffc-notifications` url

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -13,6 +13,6 @@ export default () => ({
   ffc_auth_url: "http://10.102.3.34:4002" ?? process.env.FFC_AUTH_URL,
   ffc_main_url: "http://api.ffc.mistergooddeal.org" ?? process.env.FFC_MAIN_URL,
   ffc_notifications_url:
-    "http://localhost:4003" ?? process.env.FFC_NOTIFICATIONS_URL,
+    "http://10.102.1.45:4003" ?? process.env.FFC_NOTIFICATIONS_URL,
   ffc_payments_url: "http://localhost:4004" ?? process.env.FFC_PAYMENTS_URL,
 });
